538fad25ac0f824c366f94cf4e12bc4f7db1a450
Yifeng Chen <ycpostm@gmail.com>
2018-09-20 21:35:43 -0400

    m

--- src/main/java/com/thoughtmechanix/licenseservice/LicenseController.java
+++ src/main/java/com/thoughtmechanix/licenseservice/LicenseController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/licenses")
-public class LicenseControl {
-	
+@RequestMapping(value = "/licenses")
+public class LicenseController {
+
 	@GetMapping("/{licenseId}")
 	public License getLicense(@PathVariable("licenseId") String licenseId) {
 		return new License(licenseId);
